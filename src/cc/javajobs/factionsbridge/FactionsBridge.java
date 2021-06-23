7a462fbf215a4e799805c73c744f8da9de457688
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2021-06-23 09:42:33 +0100

    Incremented Version.

--- src/cc/javajobs/factionsbridge/FactionsBridge.java
+++ src/cc/javajobs/factionsbridge/FactionsBridge.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
  */
 public class FactionsBridge implements Communicator {
 
-    private static final String version = "1.2.2";
+    private static final String version = "1.2.3";
     private static FactionsBridge instance = null;
 
     private static FactionsAPI factionapi = null;
