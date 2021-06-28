995fd1f1bf1cb4c815801dcfbe9defc43701350a
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2021-06-28 16:28:50 +0100

    Removed unrequired method to get a Relationship from Enum object.

--- src/cc/javajobs/factionsbridge/bridge/infrastructure/struct/Relationship.java
+++ src/cc/javajobs/factionsbridge/bridge/infrastructure/struct/Relationship.java
@@ -1,6 +1,5 @@
 package cc.javajobs.factionsbridge.bridge.infrastructure.struct;
 
-import cc.javajobs.factionsbridge.FactionsBridge;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -38,25 +37,6 @@ public enum Relationship {
             }
         }
         return DEFAULT_RELATIONSHIP;
-    }
-
-    /**
-     * Accessible method to convert a string into a {@link Relationship}.
-     * <p>
-     *     This method defaults to {@link #NONE}.
-     * </p>
-     *
-     * @param obj to convert
-     * @return {@link Relationship} even if there is no match.
-     */
-    @NotNull
-    public static Relationship getRelationship(@NotNull Object obj) {
-        try {
-            return obj.getClass().isEnum() ? getRelationship(((Enum<?>) obj).name()) : getRelationship(obj.toString());
-        } catch (Exception ex) {
-            FactionsBridge.get().exception(ex, "Failed to convert '" + obj + "' into valid Relationship type.");
-            return Relationship.NONE;
-        }
     }
 
 }
